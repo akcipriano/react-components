a0e0e1660f982659251999bf4ca34bb014367f96
Your Name <your_email@whatever.com>
2019-01-08 18:31:18 -0800

    Complete adding 2 nested item components

--- app.js
+++ app.js
@@ -2,11 +2,38 @@
 Inside app.js, create a GroceryList component that contains an unordered list of 2 grocery items. Render this component to the div tag in index.html with an id of app
 */// TODO
 
+
+var Apple = () => (
+  <li>apple</li>
+);
+
+var Oranges = () => (
+  <li>oranges</li>
+);
+
 var GroceryList = () => (
   <ul>
-    <li>apple</li>
-    <li>oranges</li>
+    <Oranges />
+    <Apple />
   </ul>
 );
 
+
 ReactDOM.render(<GroceryList />, document.getElementById('app'))
+
+// var GroceryList = function GroceryList() {
+//   return React.createElement(
+//     'ul',
+//     null,
+//     React.createElement(
+//       'li',
+//       null,
+//       'apple'
+//     ),
+//     React.createElement(
+//       'li',
+//       null,
+//       'oranges'
+//     )
+//   );
+// };
