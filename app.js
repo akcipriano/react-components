12494b7a8a5b906c4469d577858fe234b4fbb75a
Your Name <your_email@whatever.com>
2019-01-08 20:22:31 -0800

    Complete refactoring GroceryListItem to be a class component

--- app.js
+++ app.js
@@ -2,16 +2,24 @@
 Inside app.js, create a GroceryList component that contains an unordered list of 2 grocery items. Render this component to the div tag in index.html with an id of app
 */// TODO
 
+class GroceryListItem extends React.Component {
+  constructor(props) {
+    super(props);
+  }
+  render() {
+    console.log(this.props)
+    return (
+      <li>{this.props.Item}</li>
+    );
+  }
+}
 
-var GroceryListItem = (props) => (
+var GroceryList = (props) => (
   <ul>
-    <li>{props.groceryItems[0]}</li>
-    <li>{props.groceryItems[1]}</li>
+    {props.groceryItems.map(groceryItem =>
+      <GroceryListItem key={groceryItem} Item={groceryItem} />
+      )}
   </ul>
 );
 
-var GroceryList = () => (
-    <GroceryListItem groceryItems={['apple', 'oranges']}/>
-);
-
-ReactDOM.render(<GroceryList />, document.getElementById('app'))
+ReactDOM.render(<GroceryList groceryItems={['banana', 'cherry', 'apple']}/>, document.getElementById('app'))
